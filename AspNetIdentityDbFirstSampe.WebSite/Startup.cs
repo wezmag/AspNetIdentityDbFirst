dbd96bc718e3d64cc79a8a349d41f1474e9041c7
Wesley Huang <wezmag@gmail.com>
2017-06-16 16:05:16 +0800

    Add project files.

---
+++ AspNetIdentityDbFirstSampe.WebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetIdentityDbFirstSampe.WebSite.Startup))]
+namespace AspNetIdentityDbFirstSampe.WebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
